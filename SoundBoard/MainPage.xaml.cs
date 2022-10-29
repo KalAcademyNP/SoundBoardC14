b0c0d672d387307935c80b61124affa185fc85bb
Kal <info@kalacademy.org>
2022-10-29 11:57:22 -0700

    Completed app

--- SoundBoard/MainPage.xaml.cs
+++ SoundBoard/MainPage.xaml.cs
@@ -25,27 +25,62 @@ namespace SoundBoard
     public sealed partial class MainPage : Page
     {
         private ObservableCollection<Sound> sounds;
+        private List<MenuItem> menuItems;
         public MainPage()
         {
             this.InitializeComponent();
             sounds = new ObservableCollection<Sound>();
             SoundManager.GetAllSounds(sounds);
+
+            menuItems = new List<MenuItem>();
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/animals.png",
+                Category = SoundCategory.Animals
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/cartoon.png",
+                Category = SoundCategory.Cartoons
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/taunt.png",
+                Category = SoundCategory.Taunts
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/warning.png",
+                Category = SoundCategory.Warnings
+            });
+            BackButton.Visibility = Visibility.Collapsed;
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ContentSplitView.IsPaneOpen = !ContentSplitView.IsPaneOpen;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-
+            SoundManager.GetAllSounds(sounds);
+            CategoryTextBlock.Text = "All Sounds";
+            BackButton.Visibility = Visibility.Collapsed;
+            MenuItemsListView.SelectedItem = null;
         }
 
         private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
         {
             var sound = (Sound)e.ClickedItem;
             SoundMedia.Source = new Uri(this.BaseUri, sound.AudioFile);
+        }
+
+        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (MenuItem)e.ClickedItem;
+            CategoryTextBlock.Text = menuItem.Category.ToString();
+            SoundManager.GetAllSoundsByCategory(sounds, menuItem.Category);
+            BackButton.Visibility = Visibility.Visible;
         }
     }
 }
